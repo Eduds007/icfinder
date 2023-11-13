ae8b9fdadd437f168185981fe037c361172b4e36
Eduds007 <eduardomilanezaraujo@gmail.com>
2023-11-13 15:48:23 -0300

    add(menu): card btn

--- icfinder_app/urls.py
+++ icfinder_app/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
+    path('login/', views.login, name='login'),
 ]
--- icfinder_app/views.py
+++ icfinder_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
+
+from .models import Projeto
+
+class Index(generic.ListView):
+    model = Projeto
+    template_name = 'icfinder_app/index.html'
+    context_object_name = 'projetos'
+    
+def login (request):
     context = {}
-    return render(request, 'icfinder_app/index.html', context)
+    return render(request, 'icfinder_app/login.html', context)

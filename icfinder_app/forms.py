68ed06fa2cdf97664e24a72aeaa99cd337c59543
Vitor Viana <vitorvianadp@usp.br>
2023-11-14 11:19:21 -0300

    inputs  de email e senha na parte de login

--- icfinder_app/forms.py
+++ icfinder_app/forms.py
@@ -1,9 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Users
+from django.core.exceptions import ValidationError
+
+class CustomAuthenticationForm(AuthenticationForm):
+    widgets = {
+        'username': forms.TextInput(attrs={'style': 'font-size: 20px; background: transparent; border: none; outline: none; color: black;', 'placeholder': 'Email usp'}),
+        'password': forms.PasswordInput(attrs={'style': 'font-size: 20px; background: transparent; border: none; outline: none; color: black;', 'placeholder': 'Senha' }),
+    }
+
+    username = forms.CharField(widget=widgets['username'])
+    password = forms.CharField(widget=widgets['password'])
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = Users
         fields = ['first_name', 'last_name', 'interests', 'email', 'phone_number', 'short_bio']
+        labels = {
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'interests': 'Interesses',
+            'email': 'Email',
+            'phone_number': 'Celular',
+            'short_bio': 'Short Bio',
+        }
+
+    # aceitar apenas emails no domínio usp
+    #def clean_email(self):
+    #    email = self.cleaned_data.get('email')
+    #    if email and not email.endswith('@usp.br'):
+    #        raise ValidationError("Insira seu email USP.")
+    #    return email
 
--- icfinder_app/views.py
+++ icfinder_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-def login(request):
-    context = {}
-    return render(request, 'icfinder_app/login.html', context)
+def login_view(request):
+    if request.method == 'POST':
+        login_form = CustomAuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        login_form = CustomAuthenticationForm()
+
+    return render(
+        request,
+        'icfinder_app/login.html',
+        {'login_form': login_form}
+    )
+
+
 
 def register(request):
     if request.method == 'POST':

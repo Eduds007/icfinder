da487587069fb461e8d56b09dc6eedfb93e6456e
Vitor Viana <vitorvianadp@usp.br>
2023-11-28 13:23:04 -0300

    fix deploy render

--- icfinder_app/models.py
+++ icfinder_app/models.py
@@ -10,6 +10,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
+        user.username = email
         user.set_password(password)
         user.save(using=self._db)
         return user

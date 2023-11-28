39a5c92d605a3f9ba7d33b8d1e6b66a724138684
Vitor Viana <vitorvianadp@usp.br>
2023-11-28 10:08:41 -0300

    alteração na index

--- icfinder_app/views.py
+++ icfinder_app/views.py
@@ -248,7 +248,7 @@ class Index(LoginRequiredMixin, FilterView):
         queryset = queryset.prefetch_related(Prefetch("inscritos", queryset=InscricaoProjeto.objects.filter(estado='pendente')))
         return queryset
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['num_projetos'] = self.get_queryset().filter(self.filterset_class(self.request.GET).qs.query.where).count()
         return context
